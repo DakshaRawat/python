c01db310d62dd765e573fee3d1460a6f09cf709e
DakshaRawat <rawatdaksha2005@gmail.com>
2024-06-08 23:44:03 +0530

    sum

---
+++ class2.py
@@ -0,0 +1,52 @@
+'''value=int(input("enter"))
+print (value)
+print(type(value))
+samplefile=open('C:/Users/DAKSHA RAWAT/demo.txt','w')
+num=input("enter num")
+print("hello all{} people presnet ".format(num))
+'''
+'''str1="hi "
+str2="everyone"
+str3=str1+str2
+print(str3)'''
+'''num=20
+str1="hi"
+str2=str1+str(num)
+print (str2)
+print ("string is",str1,"datatype",type(str1))
+'''
+'''num1=20+5j
+print ("num is",num1,"datatype",type(num1))
+print(0b100)
+print(0o156)
+print(0xAFB)
+print(100+int(15.78))'''
+'''import random
+print(random.random())
+list1=["hello","my","people","kaise ho"]
+print(random.choice(list1))
+random.shuffle(list1)
+print(list1)
+'''
+'''list1=[1,2,3,"hello","bye",25.33,"true",'Go']
+print(list1[0])
+print(list1[-1])
+tup1=(1,2,3,"hello","bye",25.33,"true",'Go')
+print(tup1[0])
+print(tup1[-1])
+list1[-1]='go now'
+tup1[-1]='go now' '''
+'''
+dict1={"name":"daksha","age": 20,"salary":10000,"house no": 20}
+'''
+'''str1="hello all"  
+print(str1[-7::-2])'''
+'''str1="hello"
+str2="Hello"
+print(str1>str2)
+print(ord('h'))
+print(ord('H'))
+print(chr(104))'''
+str1="hello dear friends..."
+print(str1.find('friends'))
+
